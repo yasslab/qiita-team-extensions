c964859d993c87c60553c56f2f6d8d1ff39450bc
nalabjp <nalabjp@gmail.com>
2017-09-01 03:45:51 +0900

    Support for editing

--- default_group.user.js
+++ default_group.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Automatically select a group to publish
 // @author       nalabjp
-// @match        https://yasslab.qiita.com/drafts/new
+// @match        https://yasslab.qiita.com/drafts/*
 // @grant        none
 // ==/UserScript==
 
@@ -12,8 +12,24 @@
     'use strict';
 
     const DEFAULT_OPTION_VALUE = 'debug';
-    let selector_path = '#new_draft_item > div.editorFooter > div.editorFooter_visibilitySelector.form-inline.pull-right > select';
-    let element = document.querySelector(selector_path);
+    let selector_paths = [
+        '#new_draft_item > div.editorFooter > div.editorFooter_visibilitySelector.form-inline.pull-right > select',          // new
+        '#edit_draft_item > div.editorFooter > div.editorFooter_visibilitySelector.form-inline.pull-right > select',         // draft
+        '#edit_draft_item > div.editorFooter-onUpdate > div.editorFooter_visibilitySelector.form-inline.pull-right > select' // edit
+    ];
+    let element = null;
+
+    for(let path of selector_paths) {
+        let e = document.querySelector(path);
+        if (e !== null) {
+            element = e;
+            break;
+        }
+    }
+
     if (element === null) return;
-    element.value = DEFAULT_OPTION_VALUE;
+
+    if (element.value === '') {
+        element.value = DEFAULT_OPTION_VALUE;
+    }
 })();
