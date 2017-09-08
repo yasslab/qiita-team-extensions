2c00cedb67868dad88ff5215d5af834c9846a5a2
Seiei Miyagi <hanachin@gmail.com>
2017-09-08 10:57:23 +0900

    Fix: Do not open submit dropdown menu when edit article

--- co_editing_mode.user.js
+++ co_editing_mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Qiita:Team Extension (Co-editing mode)
 // @namespace    https://github.com/yasslab/
-// @version      0.2
+// @version      0.3
 // @description  Automatically select co-editing mode
 // @author       nalabjp
 // @match        https://yasslab.qiita.com/drafts/*
@@ -13,10 +13,8 @@
     'use strict';
 
     let dropdownToggle = document.querySelector('a.editorSubmit_dropdownToggle');
-    if (dropdownToggle === null) return;
+    let coeditingPost = document.querySelector('a.editorSubmit_dropdownItem[data-state="coediting_post"]');
+    if (dropdownToggle === null || coeditingPost === null) return;
     dropdownToggle.click();
-
-    let element = document.querySelector('a.editorSubmit_dropdownItem[data-state="coediting_post"]');
-    if (element === null) return;
-    element.click();
+    coeditingPost.click();
 })();
